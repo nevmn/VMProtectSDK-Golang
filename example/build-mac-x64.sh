d0730137e9266ca9373b428c89d9280eb306280e
Andy KoKo <90698189+czkoko@users.noreply.github.com>
2022-04-20 19:04:01 +0800

    Update build-mac-x64.sh

--- example/build-mac-x64.sh
+++ example/build-mac-x64.sh
@@ -1,3 +1,3 @@
 cd $(dirname "$0")
-CGO_LDFLAGS="-L. -lVMProtectSDK -Wl,-map,vm.map -w -s" CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 \
-go build -a -ldflags "-s -w" 
+GO111MODULE=off CGO_LDFLAGS="-L. -lVMProtectSDK -Wl,-map,vm.map -w -s" CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 \
+go build -a -ldflags "-s -w" -o vm
