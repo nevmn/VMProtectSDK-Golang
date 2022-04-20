46a0d93a187ee232be0be569fac2b96df68b7a70
Andy KoKo <90698189+czkoko@users.noreply.github.com>
2022-04-20 19:04:45 +0800

    Update build-win-x64.sh

--- example/build-win-x64.sh
+++ example/build-win-x64.sh
@@ -1,3 +1,3 @@
 cd $(dirname "$0")
-CGO_LDFLAGS="-L. -lVMProtectSDK64 -Wl,-Map,vm.map" CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 \
-go build -a -ldflags "-s -w"
+GO111MODULE=off CGO_LDFLAGS="-L. -lVMProtectSDK64 -Wl,-Map,vm.map" CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 \
+go build -a -ldflags "-s -w" -o vm.exe
