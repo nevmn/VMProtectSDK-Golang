76c3773a21cfe177b7149b5e1b613be450ddfbca
Andy KoKo <90698189+czkoko@users.noreply.github.com>
2022-04-20 19:07:30 +0800

    Add files via upload

---
+++ example/build-win-x64-llvm-mingw.bat
@@ -0,0 +1,9 @@
+@echo off
+set GO111MODULE=off
+set CC=E:\llvm-mingw\bin\clang.exe
+set CGO_LDFLAGS=-L. -lVMProtectSDK64 -Wl,-pdb=vm.pdb
+set CGO_ENABLED=1
+set GOOS=windows
+set GOARCH=amd64
+go build -a -ldflags "-s -w" -o vm.exe
+pause
