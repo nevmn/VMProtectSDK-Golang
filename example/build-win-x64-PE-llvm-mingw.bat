f5a7eeb9c0425ff3c8cd2631f96d0fcc85d561e0
Andy KoKo <90698189+czkoko@users.noreply.github.com>
2022-04-29 01:46:06 +0800

    Update build-win-x64-PE-llvm-mingw.bat

--- example/build-win-x64-PE-llvm-mingw.bat
+++ example/build-win-x64-PE-llvm-mingw.bat
@@ -1,7 +1,7 @@
 @echo off
 set GO111MODULE=off
 set CC=E:\llvm-mingw\bin\clang.exe
-set CGO_LDFLAGS=-Wl,-pdb=vm.pdb
+set CGO_LDFLAGS=-L. -lVMProtectSDK64 -Wl,-pdb=vm.pdb
 set CGO_ENABLED=1
 set GOOS=windows
 set GOARCH=amd64
