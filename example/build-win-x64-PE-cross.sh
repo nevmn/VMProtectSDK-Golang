81f6ab107d03ded321e1954b3ec0a52b36215c23
Andy KoKo <90698189+czkoko@users.noreply.github.com>
2022-04-29 01:45:40 +0800

    Update build-win-x64-PE-cross.sh

--- example/build-win-x64-PE-cross.sh
+++ example/build-win-x64-PE-cross.sh
@@ -1,3 +1,3 @@
 cd $(dirname "$0")
-GO111MODULE=off CGO_LDFLAGS="-Wl,-Map,vm.map" CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 \
+GO111MODULE=off CGO_LDFLAGS="-L. -lVMProtectSDK64 -Wl,-Map,vm.map" CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 \
 go build -a -ldflags "-s -w" -o vm.exe
